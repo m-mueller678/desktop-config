a384111c73ca0a0db55c7f494884d40800d7118c
Marcus Müller <marcus.mueller@cit.tum.de>
2023-08-17 14:40:39 +0200

    fix missing profile

--- script/keepassxc-storage-load.sh
+++ script/keepassxc-storage-load.sh
@@ -8,6 +8,6 @@ set -e -o pipefail
 cd ~/keepassxc-storage
 etag=$(aws --profile keepassxc-storage s3api head-object --bucket keepass-sync-test-5736622565ce --key db.tar|jq '.ETag' | sed 's/[^[:alnum:]]//g')
 echo $etag
-aws s3 cp s3://keepass-sync-test-5736622565ce/db.tar - | tar -x
+aws s3 cp --profile keepassxc-storage s3://keepass-sync-test-5736622565ce/db.tar - | tar -x
 echo $etag>etag
 notify-send "loaded $etag"
