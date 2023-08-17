9cb1411ea368dbc2075f538fc2a598d52a423dbb
Marcus Müller <marcus.mueller@cit.tum.de>
2023-08-17 10:12:59 +0200

    customization

--- script/autostart.sh
+++ script/autostart.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-~/script/set_screens.sh
-lxpolkit &w
+#~/script/set_screens.sh
+#lxpolkit &w
 dunst &
-pcmanfm -d --no-desktop &
-xcompmgr -c -t-5 -l-5 -r4.2 -o.55 &
-~/script/background_switch.py &
+#xcompmgr -c -t-5 -l-5 -r4.2 -o.55 &
+#~/script/background_switch.py &
 ~/script/update.sh &
-redshift &
+#redshift &
 #uget-gtk &
 
 #xprop -root -format _NET_DESKTOP_LAYOUT 32c -set _NET_DESKTOP_LAYOUT 0,3,3,0
