88cae1dbcd5f9a781aa90e08908e15adb4308297
m-mueller678 <marcus.mueller.678@gmail.com>
2025-02-23 16:58:50 +0100

    fix upload script

--- script/upload-avid.sh
+++ script/upload-avid.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+set -ex
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <url> <s3-file-name>"
+    exit 1
+fi
+
 ~/software/yt-dlp_linux --update
-echo
-echo
-echo ~/software/yt-dlp_linux "$1"
-echo gpg -r 'marcus.mueller.678@gmail.com' -o tmp.gpg --encrypt "<in-name>"
-echo aws s3 cp tmp.gpg 's3://backups-avid/'"<s3-name>".gpg --storage-class DEEP_ARCHIVE
+FILE_NAME="$(~/software/yt-dlp_linux --quiet --no-warnings --get-filename --no-simulate "$1")"
+gpg -r 'marcus.mueller.678@gmail.com' -o tmp.gpg --encrypt "$FILE_NAME"
+aws s3 cp tmp.gpg 's3://backups-avid/'"$2".gpg --storage-class DEEP_ARCHIVE
+
 echo 'https://eu-west-1.console.aws.amazon.com/s3/buckets/backups-avid'
